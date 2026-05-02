4f72c382a5c09f50ab0406e8d005114c6f2d2ba3
Arthur Zheng <arthurzheng150@gmail.com>
2026-05-02 13:17:49 +0800

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# Chinese TTS
+
+![Version](https://img.shields.io/github/v/tag/openblock-plugin/chineseTTS?label=version)
+![License](https://img.shields.io/badge/license-MIT-green.svg)
+![Type](https://img.shields.io/badge/type-extension-orange.svg)
+![Tags](https://img.shields.io/badge/tags-actuator-lightgrey.svg)
+
+Text to speech module based on SYN6288, support Chinese and English letters and numbers.
+
+## Documentation
+
+For detailed usage instructions, please visit:
+
+**[OpenBlock Wiki](https://wiki.openblock.cc)**
---
+++ src/translations.js
@@ -0,0 +1,95 @@
+/* eslint-disable quote-props */
+/* eslint-disable max-len */
+/**
+ * Translation file for this resouce.
+ *
+ * IMPORTANT:
+ * - The "en" (English) section is automatically generated from source files.
+ *   Do NOT modify the "en" section manually.
+ * - Other language sections (e.g., "zh-cn", "zh-tw", "ja", etc.) should be
+ *   manually translated by you.
+ * - When you run the extraction script again, only the "en" section will be
+ *   updated. Your manual translations in other languages will be preserved.
+ *
+ * Structure:
+ * - interface: translations for name/description (used by GUI formatMessage)
+ * - extensions: translations for extension blocks (used by VM formatMessage)
+ * - blocks: translations for Blockly blocks (used by Blockly.Msg)
+ */
+
+export default {
+    'interface': {
+        'en': {
+            'chineseTTS.name': 'Chinese TTS',
+            'chineseTTS.description': 'Text to speech module based on SYN6288, support Chinese and English letters and numbers.'
+        },
+        'ru': {
+            'chineseTTS.name': 'Chinese TTS',
+            'chineseTTS.description': 'Text to speech module based on SYN6288, support Chinese and English letters and numbers.'
+        },
+        'zh-cn': {
+            'chineseTTS.name': 'Chinese TTS',
+            'chineseTTS.description': 'Text to speech module based on SYN6288, support Chinese and English letters and numbers.'
+        },
+        'zh-tw': {
+            'chineseTTS.name': 'Chinese TTS',
+            'chineseTTS.description': 'Text to speech module based on SYN6288, support Chinese and English letters and numbers.'
+        }
+    },
+    'extensions': {
+        'en': {},
+        'ru': {},
+        'zh-cn': {},
+        'zh-tw': {}
+    },
+    'blocks': {
+        'en': {
+            'CHINESETTS_CATEGORY': 'Chinese TTS',
+            'CHINESETTS_INIT': 'init chinese TTS module pin RX %1 TX %2',
+            'CHINESETTS_SAY': 'chinese TTS module say %1',
+            'CHINESETTS_SAYUNTILLFINISH': 'chinese TTS module say %1 untill finish',
+            'CHINESETTS_PLAYSOUND': 'chinese TTS module play sound %1',
+            'CHINESETTS_PLAYSOUNDUNTILLFINISH': 'chinese TTS module play sound %1 untill finish',
+            'CHINESETTS_SETVOLUME': 'chinese TTS module set volume %1',
+            'CHINESETTS_SETSPEECHRATE': 'chinese TTS module set speech rate %1',
+            'CHINESETTS_SETPOWERONPROMPT': 'chinese TTS module set sound of power on prompt %1',
+            'CHINESETTS_STATE_OFF': 'off'
+        },
+        'ru': {
+            'CHINESETTS_CATEGORY': 'Chinese TTS',
+            'CHINESETTS_INIT': 'init chinese TTS module pin RX %1 TX %2',
+            'CHINESETTS_SAY': 'chinese TTS module say %1',
+            'CHINESETTS_SAYUNTILLFINISH': 'chinese TTS module say %1 untill finish',
+            'CHINESETTS_PLAYSOUND': 'chinese TTS module play sound %1',
+            'CHINESETTS_PLAYSOUNDUNTILLFINISH': 'chinese TTS module play sound %1 untill finish',
+            'CHINESETTS_SETVOLUME': 'chinese TTS module set volume %1',
+            'CHINESETTS_SETSPEECHRATE': 'chinese TTS module set speech rate %1',
+            'CHINESETTS_SETPOWERONPROMPT': 'chinese TTS module set sound of power on prompt %1',
+            'CHINESETTS_STATE_OFF': 'off'
+        },
+        'zh-cn': {
+            'CHINESETTS_CATEGORY': 'Chinese TTS',
+            'CHINESETTS_INIT': 'init chinese TTS module pin RX %1 TX %2',
+            'CHINESETTS_SAY': 'chinese TTS module say %1',
+            'CHINESETTS_SAYUNTILLFINISH': 'chinese TTS module say %1 untill finish',
+            'CHINESETTS_PLAYSOUND': 'chinese TTS module play sound %1',
+            'CHINESETTS_PLAYSOUNDUNTILLFINISH': 'chinese TTS module play sound %1 untill finish',
+            'CHINESETTS_SETVOLUME': 'chinese TTS module set volume %1',
+            'CHINESETTS_SETSPEECHRATE': 'chinese TTS module set speech rate %1',
+            'CHINESETTS_SETPOWERONPROMPT': 'chinese TTS module set sound of power on prompt %1',
+            'CHINESETTS_STATE_OFF': 'off'
+        },
+        'zh-tw': {
+            'CHINESETTS_CATEGORY': 'Chinese TTS',
+            'CHINESETTS_INIT': 'init chinese TTS module pin RX %1 TX %2',
+            'CHINESETTS_SAY': 'chinese TTS module say %1',
+            'CHINESETTS_SAYUNTILLFINISH': 'chinese TTS module say %1 untill finish',
+            'CHINESETTS_PLAYSOUND': 'chinese TTS module play sound %1',
+            'CHINESETTS_PLAYSOUNDUNTILLFINISH': 'chinese TTS module play sound %1 untill finish',
+            'CHINESETTS_SETVOLUME': 'chinese TTS module set volume %1',
+            'CHINESETTS_SETSPEECHRATE': 'chinese TTS module set speech rate %1',
+            'CHINESETTS_SETPOWERONPROMPT': 'chinese TTS module set sound of power on prompt %1',
+            'CHINESETTS_STATE_OFF': 'off'
+        }
+    }
+};
